bd35cd255efc37baa661d18504050fdeb37f3703
devwlc <wlcferreira.dev@gmail.com>
2026-06-14 14:53:31 -0300

    feat(validation): configure envValidationSchema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { PrismaService } from '@/src/database/prisma.service';
+import { envValidationSchema } from '@/src/config/env.validation';
 import { AuthModule } from '@/modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: envValidationSchema,
     }),
     AuthModule,
   ],
